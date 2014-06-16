e3f7231a0b66f154c015faa1ab804a81b7c44799
roronya <roronya@roronya-ThinkPadT410.(none)>
2014-06-16 11:25:27 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# twitterbot-clj
+# automatic-sentence-generator-clj
 
 A Clojure library designed to ... well, that part is up to you.
 
--- src/twitterbot_clj/core.clj
+++ src/twitterbot_clj/core.clj
@@ -2,10 +2,13 @@
   (:import [org.atilika.kuromoji Token Tokenizer])
   (:use [clojure.string :only (split)]))
 
+(defn split-by-period [paragraph]
+  (map #(str %1 "。") (split paragraph #"。")))
+
 (defn morphological-analysis-sentence [sentence]
-  (let [tokenizer (-> (Tokenizer/builder) .build )
+  (let [tokenizer (-> (Tokenizer/builder) .build)
         token (.tokenize tokenizer sentence)]
-    (concat ["-start"] (map #(.getSurfaceForm %1) token) ["-end"])))
+    (concat ["-start" ] (map #(.getSurfaceForm %1) token) ["-end"])))
 
 (defn create-markov-chunk [morphemes]
   (let [chunkes (ref [])]
@@ -16,15 +19,18 @@
           (recur (rest morphemes)))))
     @chunkes))
 
-(defn create-frequency-list [morkov-chunk]
-  (let [frequency-list (ref {})]
-    (doseq [chunk morkov-chunk]
-      (when (nil? (keyword (first chunk)) frequency-list)
-        (dosync alter frequency-list assoc (keyword (first chunk)) [(chunk 0)])))))
+(def frequency-list (ref {}))
+
+(defn add-frequency-list [markov-chunk]
+  (doseq [markov-chunk-element markov-chunk]
+    (cond (nil? (get @frequency-list (first markov-chunk-element)))
+          (dosync (alter frequency-list assoc (first markov-chunk-element) {markov-chunk-element 1}))
+          (nil? (get (get @frequency-list (first markov-chunk-element)) markov-chunk-element))
+          (dosync (alter frequency-list update-in [(first markov-chunk-element)] assoc markov-chunk-element 1))
+          :else
+          (dosync (alter frequency-list update-in [(first markov-chunk-element) markov-chunk-element] inc)))))
+
+(get @frequency-list "-start")
+(map #(add-frequency-list (create-markov-chunk (morphological-analysis-sentence %1))) (split-by-period "吾輩は猫である。名前はまだない。"))
 
 
-(first (create-markov-chunk (morphological-analysis-sentence "吾輩は猫である。")))
-(comment
-  ("-start" "吾輩" "は") => {"start" {'("-start" "吾輩" "は") 1}})
-
-(assoc {} :a 1)
